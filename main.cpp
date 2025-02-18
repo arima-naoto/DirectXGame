b4472673054c09df5952b25ffa53dea0fad0860d
arima-naoto <k023g1010@g.neec.ac.jp>
2025-02-19 00:20:32 +0900

    コードをからっぽにした

--- main.cpp
+++ main.cpp
@@ -1,51 +1,6 @@
-﻿#include "WinApp.h"
-#include "DirectXCommon.h"
-#include "ImGuiManager.h"
-#include "Input.h"
-#include "Audio.h"
+﻿#include "Windows.h"
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-
-	WinApp* win = nullptr;
-	DirectXCommon* dxCommon = nullptr;
-	Input* input = nullptr;
-	Audio* audio = nullptr;
-
-	win = WinApp::GetInstance();
-	win->CreateGameWindow();
-
-	dxCommon = DirectXCommon::GetInstance();
-	dxCommon->Initialize(win);
-
-	ImGuiManager* imguiManager = ImGuiManager::GetInstance();
-	imguiManager->Initialize(win, dxCommon);
-
-	input = Input::GetInstance();
-	input->Initialize();
-
-	audio = Audio::GetInstance();
-	audio->Initialize();
-
-	while (true) {
-		if (win->ProcessMessage()) {
-			break;
-		}
-
-		imguiManager->Begin();
-
-		input->Update();
-
-		imguiManager->End();
-
-		dxCommon->BeginDraw();
-		imguiManager->Draw();
-		
-		dxCommon->EndDraw();
-	}
-
-	audio->Finalize();
-	imguiManager->Finalize();
-	win->Terminate();
 
 	return 0;
 }
--- math/Vector2.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-
-/// <summary>
-/// 二次元ベクトル
-/// </summary>
-struct Vector2 {
-	float x;
-	float y;
-};
